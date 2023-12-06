850bdde2d1c988a863ca1d033fd1963facbe68d6
Muhammad Fachry Ramadhan <fachry.ramadhan@equine.co.id>
2023-12-06 16:15:36 +0700

    Update

--- Scripts/Skenario/N1_Login Without User/Script1701853641994.groovy
+++ Scripts/Skenario/N1_Login Without User/Script1701853641994.groovy
@@ -17,12 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.callTestCase(findTestCase('Page/Login/Login'), [
-	
-	('username'): Username,
-	('password'): Password
-
-	], FailureHandling.STOP_ON_FAILURE)
+WebUI.callTestCase(findTestCase('Page/Login/Login'), [('username') : Username, ('password') : Password], FailureHandling.STOP_ON_FAILURE)
 
 WebUI.delay(2)
 
